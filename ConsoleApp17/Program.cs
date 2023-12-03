00705d03502ecdfd2869e1a8d504e336fae85021
aiymgulasylbekova <124632132+aiymgulasylbekova@users.noreply.github.com>
2023-12-03 23:19:25 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+
+        static Dictionary<string, int> CountWordFrequency(string text)
+        {
+
+            string[] words = text.Split(new[] { ' ', '.', ',', '-', '­', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+            Dictionary<string, int> wordFrequency = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+
+
+            foreach (var word in words)
+            {
+                if (wordFrequency.ContainsKey(word))
+                {
+                    wordFrequency[word]++;
+                }
+                else
+                {
+                    wordFrequency[word] = 1;
+                }
+            }
+
+            return wordFrequency;
+        }
+        static void Main()
+        {
+            string text = "Вот дом, Который построил Джек. А это пшеница, Которая в темном чулане хранится " +
+                          "В доме, Который построил Джек. А это веселая птица­синица, Которая часто ворует " +
+                          "пшеницу, Которая в темном чулане хранится В доме, Который построил Джек.";
+
+            Dictionary<string, int> wordFrequency = CountWordFrequency(text);
+
+            Console.WriteLine("Слово\t\tЧастота");
+            Console.WriteLine("------------------------");
+            foreach (var pair in wordFrequency)
+            {
+                Console.WriteLine($"{pair.Key}\t\t{pair.Value}");
+            }
+            Console.ReadKey();
+        }
+
+        
+    }
+
+}
